3d5a071023bf69ce2b328ca0065feebe3f2bebc2
unknown <bongekam16@gmail.com>
2023-11-29 10:55:07 +0200

    initial commit

---
+++ SRc/script.js
@@ -0,0 +1,9 @@
+function subscribe(){
+    document.getElementsByClassName("maindiv")[0].style.display = "none"
+    document.getElementsByClassName("main2")[0].style.display = "block"
+}
+
+function dismiss(){
+    document.getElementsByClassName("maindiv")[0].style.display = "flex"
+    document.getElementsByClassName("main2")[0].style.display = "none"
+}
